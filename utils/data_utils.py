d4b7b48c37a35950277e34a3922aa623c2b4546a
Ata Goker <138634148+atagoker@users.noreply.github.com>
2024-06-27 15:36:46 +0200

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,1 +1,15 @@
+import os
+import json
 
+current_dir = os.path.dirname(__file__)
+raw_data_dir = os.path.join(current_dir, '..', 'data', 'datasets', 'raw')
+
+def load_json(filename):
+    file_path = os.path.join(raw_data_dir, filename)
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def save_json(data, filename):
+    file_path = os.path.join(raw_data_dir, filename)
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
