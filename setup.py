b5575a628acf2fb3fd49139541a0625539147e9e
Ata Goker <138634148+atagoker@users.noreply.github.com>
2024-06-27 16:24:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='music_recommendation_system',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'flask',
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'scipy',
+        'python-dotenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'music_recommendation_system=main:main',
+        ],
+    },
+)
