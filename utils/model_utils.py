6618b5542a0c5aa0e85145c9f5509101ccf4812e
Ata Goker <138634148+atagoker@users.noreply.github.com>
2024-06-27 15:37:49 +0200

    Update model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,1 +1,9 @@
+import pickle
 
+def save_model(model, filename):
+    with open(filename, 'wb') as file:
+        pickle.dump(model, file)
+
+def load_model(filename):
+    with open(filename, 'rb') as file:
+        return pickle.load(file)
