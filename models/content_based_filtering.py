e5ebb31f725e5bbfc967c958ed1a20de19bb67d1
Ata Goker <138634148+atagoker@users.noreply.github.com>
2024-06-27 15:35:22 +0200

    Update content_based_filtering.py

--- models/content_based_filtering.py
+++ models/content_based_filtering.py
@@ -1,1 +1,14 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
+class ContentBasedFiltering:
+    def __init__(self):
+        self.item_similarity = None
+    
+    def fit(self, item_features):
+        self.item_similarity = cosine_similarity(item_features)
+    
+    def recommend(self, item_id, n_recommendations=5):
+        similar_items = np.argsort(self.item_similarity[item_id])[::-1]
+        return similar_items[:n_recommendations]
